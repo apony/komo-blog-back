99bce6457ee6666dd171569892fbd777f67688e0
Jack <13172211447@163.com>
2019-10-11 01:43:40 +0800

    mongodb连接兼容

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 // 先摸过来
-const Koa = require('koa');
-const config = require('./config');
+const Koa = require('koa')
+const config = require('./config')
 
 // https://www.npmjs.com/package/koa2-cors
-const cors = require('koa2-cors');
+const cors = require('koa2-cors')
 
 // https://www.npmjs.com/package/koa-bodyparser
-const bodyParser = require('koa-bodyparser');
+const bodyParser = require('koa-bodyparser')
 
 // https://github.com/Automattic/mongoose
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const app = new Koa();
+const app = new Koa()
 
-mongoose.connect(config.db, {useNewUrlParser:true}, (err) => {
+mongoose.set('useCreateIndex', true)
+mongoose.connect(
+  config.db,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
     if (err) {
-        console.error('Failed to connect to database');
+      console.log(err)
+      console.error('Failed to connect to database')
     } else {
-        console.log('Connecting database successfully');
+      console.log('Connecting database successfully')
     }
-});
+  }
+)
 
-app.use(cors());
-app.use(bodyParser());
+app.use(cors())
+app.use(bodyParser())
 
 /*自由发挥区-start*/
-const test_router = require('./routes/api/test_router');
-const user_router = require('./routes/api/user_router');
+const test_router = require('./routes/api/test_router')
+const user_router = require('./routes/api/user_router')
 
-app.use(test_router.routes()).use(test_router.allowedMethods());
-app.use(user_router.routes()).use(user_router.allowedMethods());
+app.use(test_router.routes()).use(test_router.allowedMethods())
+app.use(user_router.routes()).use(user_router.allowedMethods())
 /*end*/
 
-app.listen(config.port);
+app.listen(config.port)
