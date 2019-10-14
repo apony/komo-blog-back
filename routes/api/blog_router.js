bb7cfea66aaa7e7997d988e96ec20b195ee52ee5
Jack <13172211447@163.com>
2019-10-15 00:31:54 +0800

    pretter

--- routes/api/blog_router.js
+++ routes/api/blog_router.js
@@ -1,8 +1,8 @@
-const Router = require('koa-router');
-const router = new Router();
-const blog_controller = require('./../../app/controllers/blog_controller');
+const Router = require('koa-router')
+const router = new Router()
+const blog_controller = require('./../../app/controllers/blog_controller')
 
-router.get('/blog/get', blog_controller.get);
-router.post('/blog/add', blog_controller.add);
+router.get('/blog/get', blog_controller.get)
+router.post('/blog/add', blog_controller.add)
 
-module.exports = router;
+module.exports = router
